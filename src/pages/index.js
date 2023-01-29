c36894b51aa7fa92b6d9190a3303368d5ebd7cb8
Sinnamoon <joan.glendinning@ynov.com>
2023-01-29 21:05:29 +0100

    font

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       <main className='bg-white px-10'>
       <section className='  min-h-screen'>
         <nav className='bg-pink-100 rounded-full px-10 py-10 mb-12 flex justify-between dark:text-white'>
-          <h1 className='font-burtons text-xl text-black'> Dev by joni </h1>
+          <h1 className='text-xl text-black font-burtons'> Developped by J.G ♥ </h1>
             <ul className='flex items-center'>
               <li> <BsFillMoonStarsFill className='text-black'/></li>
               <li> <a className='bg-gradient-to-r from-pink-400 to-teal-400 text-white px-4 py-2 rounded-full ml-8' href='#'> CV </a></li>
