3b4fd5e7ee078efe71a0c4736863f9d8b0d39af9
Joan G <jonisouthanva@gmail.com>
2023-01-29 19:23:49 +0100

    nav

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-
+import {BsFillMoonStarsFill} from 'react-icons/bs';
+import {AiFillTwitterCircle, AiFillLinkedin, AiFillGithub} from 'react-icons/ai';
 
 export default function Home() {
   return (
@@ -10,7 +11,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head> 
 
-      <main></main>      
+      <main className='bg-white px-10'>
+      <section className='  min-h-screen'>
+        <nav className='bg-pink-100 rounded-full px-10 py-10 mb-12 flex justify-between dark:text-white'>
+          <h1 className='font-burtons text-xl text-black'> Dev by joni </h1>
+            <ul className='flex items-center'>
+              <li> <BsFillMoonStarsFill className='text-black'/></li>
+              <li> <a className='bg-gradient-to-r from-pink-400 to-teal-400 text-white px-4 py-2 rounded-full ml-8' href='#'> CV </a></li>
+            </ul>
+        </nav>
+        sus
+      </section>
+      </main>      
     </div>
   );
 }
